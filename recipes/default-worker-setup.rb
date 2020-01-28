a6d82d9b76bcbd00d5b196a86bf21522f9973832
Narunas-K <narunas.kapocius@gmail.com>
2020-01-28 09:59:39 +0200

    Added default-worker-setup.rb

---
+++ recipes/default-worker-setup.rb
@@ -0,0 +1,27 @@
+directory '/tmp/phys-tcp-throughput-test/sar-results' do
+  recursive true
+  owner 'narunas'
+  group 'narunas'
+  mode '0755'
+end
+
+directory '/tmp/phys-tcp-throughput-test/sar-logs' do
+  recursive true
+  owner 'narunas'
+  group 'narunas'
+  mode '0755'
+end
+
+directory '/tmp/phys-tcp-throughput-test/iperf-results' do
+  recursive true
+  owner 'narunas'
+  group 'narunas'
+  mode '0755'
+end
+
+directory '/tmp/phys-tcp-throughput-test' do
+  recursive true
+  owner 'narunas'
+  group 'narunas'
+  mode '0755'
+end
